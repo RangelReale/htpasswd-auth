6cd1e7f31e3623efb1bc99257bf31e3020ebaa83
Rangel Reale <rangelspam@gmail.com>
2020-09-24 15:22:28 -0300

    implement check

--- app/controllers/check.go
+++ app/controllers/check.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"encoding/base64"
+	"github.com/RangelReale/htpasswd-auth/app"
 	"github.com/revel/revel"
+	"net/http"
+	"strings"
 )
 
 type Check struct {
@@ -9,5 +13,40 @@ type Check struct {
 }
 
 func (c Check) Index() revel.Result {
-	return c.Render()
+	cookie, err := c.Request.Cookie(revel.Config.StringDefault("htpa.cookieName", "htpa_auth"))
+	if err == nil {
+		if cookie != nil && cookie.GetValue() != "" {
+			c.Log.Warnf("Cookie value: %s", cookie.GetValue())
+			authstr, err := base64.StdEncoding.DecodeString(cookie.GetValue())
+			if err == nil {
+				authpair := strings.SplitN(string(authstr), ":", 2)
+				if len(authpair) == 2 {
+					if app.HtPasswd.Match(authpair[0], authpair[1]) {
+						c.Response.Status = http.StatusOK
+						unheader := revel.Config.StringDefault("htpa.usernameHeader", "")
+						if unheader != "" {
+							c.Response.Out.Header().Add(unheader, authpair[0])
+						}
+
+						return c.RenderText("OK")
+					}
+				} else {
+					c.Log.Warn("Cookie must have exactly 2 items")
+				}
+			} else {
+				c.Log.Warnf("Error decoding cookie: %s", err.Error())
+			}
+		}
+	} else {
+		if err != http.ErrNoCookie {
+			c.Log.Warnf("Error reading cookie: %s", err.Error())
+		}
+	}
+
+	authUrl := revel.Config.StringDefault("htpa.authUrl", "")
+	if strings.TrimSpace(authUrl) != "" {
+		return c.Redirect(authUrl)
+	}
+	c.Response.Status = http.StatusForbidden
+	return c.RenderText("Forbidden")
 }
